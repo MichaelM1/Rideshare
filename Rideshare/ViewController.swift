dfd649663c70a2af493e758743f0ee045e66f474
Michael Mao <michaelmao1999@gmail.com>
2019-10-26 15:07:23 -0700

    Initial Commit

---
+++ Rideshare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rideshare
+//
+//  Created by Michael Mao on 10/26/19.
+//  Copyright © 2019 Michael Mao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
